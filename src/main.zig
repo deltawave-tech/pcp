80d6133df139fb71528420c60701800f52059768
Thomas Bach <info@thomasbach.dev>
2025-05-23 16:18:41 +0200

    refactor: use generalized adam optimizer in gpt2

--- src/main.zig
+++ src/main.zig
@@ -6,9 +6,14 @@ pub const autodiff = @import("autodiff.zig");
 pub const ops = @import("ops.zig");
 pub const metal = @import("backends/metal.zig");
 
+pub const optimizers = struct {
+    pub usingnamespace @import("optimizers/adam.zig");
+};
+
 test {
     // Import all tests from modules
     _ = @import("tensor.zig");
     _ = @import("autodiff.zig");
     _ = @import("ops.zig");
+    _ = @import("optimizers/adam.zig");
 }
